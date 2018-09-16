d1ee5401a6cef1055ef51c7c576ca0d3be035c3b
sleevan <522400740@qq.com>
2018-09-16 22:18:37 +0800

    修改vm

--- src/main/java/com/gen/demo/Main.java
+++ src/main/java/com/gen/demo/Main.java
@@ -11,6 +11,9 @@ import com.gen.demo.assist.JVP;
 public class Main {
 
     public static final JVP DAO = new JVP();
+    public static final JVP DAO_IMPL = new JVP();
+    public static final JVP SERVICE = new JVP();
+    public static final JVP SERVICE_IMPL = new JVP();
 
     static {
 
@@ -19,8 +22,21 @@ public class Main {
         DAO.setVmName("beanDao.vm");
         DAO.setDesc("说明");
         DAO.setJavaFilePath("/genFile/");
+        //生成dao代码
+        DAO_IMPL.setJavaSuffix("DaoImpl.java");
+        DAO_IMPL.setVmName("beanDaoImpl.vm");
+        DAO_IMPL.setDesc("说明");
+        DAO_IMPL.setJavaFilePath("/genFile/");
 
+        SERVICE.setJavaSuffix("Service.java");
+        SERVICE.setVmName("beanService.vm");
+        SERVICE.setDesc("说明");
+        SERVICE.setJavaFilePath("/genFile/");
 
+        SERVICE_IMPL.setJavaSuffix("ServiceImpl.java");
+        SERVICE_IMPL.setVmName("beanServiceImpl.vm");
+        SERVICE_IMPL.setDesc("说明");
+        SERVICE_IMPL.setJavaFilePath("/genFile/");
     }
 
     private static Main ins = new Main();
@@ -33,6 +49,9 @@ public class Main {
 
     public void beanTool(BeanUtils beanUtils, String targetEntity) throws Exception {
         beanUtils.createBeanFiles(targetEntity, Main.DAO);
+        beanUtils.createBeanFiles(targetEntity, Main.DAO_IMPL);
+        beanUtils.createBeanFiles(targetEntity, Main.SERVICE);
+        beanUtils.createBeanFiles(targetEntity, Main.SERVICE_IMPL);
     }
 }
 
